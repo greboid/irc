40723fb8a5cfd1b4191a25fdc1463d549c636bc8
Greg Holmes <git@greg.holmes.name>
2020-02-27 18:59:05 +0000

    sprintf port into web server

--- web.go
+++ web.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/greboid/irc/irc"
 	"github.com/labstack/echo"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 type Web struct {
 	channel string
 	port    int
-	irc     irc.IRCConnection
+	irc     *irc.IRCConnection
 }
 
 func (web *Web) StartWeb() {
@@ -19,7 +19,7 @@ func (web *Web) StartWeb() {
 	e.HideBanner = true
 	e.HidePort = true
 	e.GET("/", web.rootPath)
-	err := e.Start("0.0.0.0:" + strconv.Itoa(web.port))
+	err := e.Start(fmt.Sprintf("0.0.0.0:%d", web.port))
 	if err != nil {
 		log.Panicf("Unable to start web server: %v", err)
 	}
