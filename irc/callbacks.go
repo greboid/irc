551cf418b2ec0acc54c58dac87ad8a77e21e7e1a
Greg Holmes <git@greg.holmes.name>
2020-02-29 04:03:11 +0000

    Fix PONG handling

--- irc/callbacks.go
+++ irc/callbacks.go
@@ -33,7 +33,7 @@ func (irc *Connection) runCallbacks(m *Message) {
 }
 
 func pong(c *Connection, m *Message) {
-	c.SendRawf("PONG :%v", m.ParamsArray[1])
+	c.SendRawf("PONG :%v", m.ParamsArray[0])
 }
 
 func quitOnError(c *Connection, _ *Message) {
