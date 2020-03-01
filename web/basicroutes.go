7ef1852c1bd20eb30fbcde6e4e97a7ea8b7c52e7
Greg Holmes <git@greg.holmes.name>
2020-03-01 17:04:29 +0000

    Add a github hook endpoint

--- web/basicroutes.go
+++ web/basicroutes.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"github.com/greboid/irc/bot"
 	"github.com/labstack/echo"
 	"net/http"
 )
@@ -8,10 +9,19 @@ import (
 func addBasicRoutes(e *echo.Echo, web *Web) {
 	user := e.Group("/user/:key", userKeyMiddleware(web.db))
 	user.GET("/message/:message", web.sendMessage)
+	user.POST("/github/webhook", web.github)
 }
 
 func (web *Web) sendMessage(context echo.Context) error {
 	message := context.Param("message")
 	go web.irc.SendRawf("PRIVMSG %s :%s", web.conf.Channel, message)
 	return context.String(http.StatusOK, "Done")
+}
+
+func (web *Web) github(context echo.Context) (err error) {
+	wh := new(bot.GitHubCommitHook)
+	if err = context.Bind(wh); err != nil {
+		return
+	}
+	return context.JSON(http.StatusOK, wh)
 }
