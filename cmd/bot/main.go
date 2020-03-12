5c0c44be85e6327e6e494b31837e138363f35af2
Greg Holmes <git@greg.holmes.name>
2020-03-12 22:54:40 +0000

    rename grpc-server

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"github.com/greboid/irc/config"
 	"github.com/greboid/irc/database"
-	grpcserver "github.com/greboid/irc/grpc-server"
 	"github.com/greboid/irc/irc"
+	"github.com/greboid/irc/rpc"
 	"github.com/greboid/irc/web"
 	"log"
 )
@@ -15,13 +15,13 @@ func main() {
 	conf := config.GetConfig()
 	db := database.New(conf.DBPath)
 	connection := irc.NewIRC(conf)
-	grpc := grpcserver.GrpcServer{Conn: connection, DB: db}
+	rpcServer := rpc.GrpcServer{Conn: connection, DB: db}
 	go web.NewWeb(conf, connection, db).StartWeb()
 	log.Print("Adding callbacks")
 	connection.AddInboundHandler("001", func(c *irc.Connection, m *irc.Message) {
 		c.SendRawf("JOIN :%s", conf.Channel)
 	})
-	go grpc.StartGRPC()
+	go rpcServer.StartGRPC()
 	err := connection.ConnectAndWait()
 	if err != nil {
 		log.Fatal(err)
