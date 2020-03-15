cde2d93b7ab9d4017b690bf7575951948051af7f
Greg Holmes <git@greg.holmes.name>
2020-03-15 16:10:59 +0000

    Rename connections from b to c

--- irc/messageBus.go
+++ irc/messageBus.go
@@ -9,50 +9,50 @@ func newExternalBus(size int) externalbus.MessageBus {
 	return mb
 }
 
-func (b *Connection) SubscribeCapAdd(receiver func(*Connection, *CapabilityStruct)) {
-	_ = b.bus.Subscribe("+cap", receiver)
+func (c *Connection) SubscribeCapAdd(receiver func(*Connection, *CapabilityStruct)) {
+	_ = c.bus.Subscribe("+cap", receiver)
 }
 
-func (b *Connection) UnsubscribeCapAdd(receiver func(*Connection, *CapabilityStruct)) {
-	_ = b.bus.Unsubscribe("+cap", receiver)
+func (c *Connection) UnsubscribeCapAdd(receiver func(*Connection, *CapabilityStruct)) {
+	_ = c.bus.Unsubscribe("+cap", receiver)
 }
 
-func (b *Connection) PublishCapAdd(conn *Connection, capability *CapabilityStruct) {
-	b.bus.Publish("+cap", conn, capability)
+func (c *Connection) PublishCapAdd(conn *Connection, capability *CapabilityStruct) {
+	c.bus.Publish("+cap", conn, capability)
 }
 
-func (b *Connection) SubscribeCapDel(receiver func(*Connection, *CapabilityStruct)) {
-	_ = b.bus.Subscribe("-cap", receiver)
+func (c *Connection) SubscribeCapDel(receiver func(*Connection, *CapabilityStruct)) {
+	_ = c.bus.Subscribe("-cap", receiver)
 }
 
-func (b *Connection) UnsubscribeCapDel(receiver func(*Connection, *CapabilityStruct)) {
-	_ = b.bus.Unsubscribe("-cap", receiver)
+func (c *Connection) UnsubscribeCapDel(receiver func(*Connection, *CapabilityStruct)) {
+	_ = c.bus.Unsubscribe("-cap", receiver)
 }
 
-func (b *Connection) PublishCapDel(conn *Connection, capability *CapabilityStruct) {
-	b.bus.Publish("-cap", conn, capability)
+func (c *Connection) PublishCapDel(conn *Connection, capability *CapabilityStruct) {
+	c.bus.Publish("-cap", conn, capability)
 }
 
-func (b *Connection) SubscribeChannelPart(receiver func(Channel)) {
-	_ = b.bus.Subscribe("ChannelPart", receiver)
+func (c *Connection) SubscribeChannelPart(receiver func(Channel)) {
+	_ = c.bus.Subscribe("ChannelPart", receiver)
 }
 
-func (b *Connection) UnsubscribeChannelPart(receiver func(Channel)) {
-	_ = b.bus.Unsubscribe("ChannelPart", receiver)
+func (c *Connection) UnsubscribeChannelPart(receiver func(Channel)) {
+	_ = c.bus.Unsubscribe("ChannelPart", receiver)
 }
 
-func (b *Connection) PublishChannelPart(channel Channel) {
-	b.bus.Publish("ChannelPart", channel)
+func (c *Connection) PublishChannelPart(channel Channel) {
+	c.bus.Publish("ChannelPart", channel)
 }
 
-func (b *Connection) SubscribeChannelMessage(receiver func(Message)) {
-	_ = b.bus.Subscribe("ChannelMessage", receiver)
+func (c *Connection) SubscribeChannelMessage(receiver func(Message)) {
+	_ = c.bus.Subscribe("ChannelMessage", receiver)
 }
 
-func (b *Connection) UnsubscribeChannelMessage(receiver func(Message)) {
-	_ = b.bus.Unsubscribe("ChannelMessage", receiver)
+func (c *Connection) UnsubscribeChannelMessage(receiver func(Message)) {
+	_ = c.bus.Unsubscribe("ChannelMessage", receiver)
 }
 
-func (b *Connection) PublishChannelMessage(channel Message) {
-	b.bus.Publish("ChannelMessage", channel)
+func (c *Connection) PublishChannelMessage(channel Message) {
+	c.bus.Publish("ChannelMessage", channel)
 }
