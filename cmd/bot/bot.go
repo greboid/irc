91f3f02b98f7b1a64650b08949d3eb50cdf00f4f
Greg Holmes <git@greg.holmes.name>
2020-09-15 21:58:31 +0100

    Support keyed channels

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/greboid/irc/irc"
 	"strings"
 )
@@ -11,10 +12,32 @@ func addBotCallbacks(c *irc.Connection) {
 }
 
 func joinChannels(_ *irc.EventManager, c *irc.Connection, _ *irc.Message) {
-	channels := strings.Split(*Channel, ",")
-	for index := range channels {
-		c.SendRawf("JOIN :%s", channels[index])
+	for _, join := range getJoinCommands(*Channel) {
+		c.SendRawf(join)
 	}
+}
+
+func getJoinCommands(channelString string) (joinCommands []string) {
+	keyedChannels := make([]string, 0)
+	keys := make([]string, 0)
+	keylessChannels := make([]string, 0)
+	channels := strings.Split(channelString, ",")
+	for index := range channels {
+		parts := strings.Split(channels[index], "=")
+		if len(parts) == 1 {
+			keylessChannels = append(keylessChannels, channels[index])
+		} else if len(parts) == 2 {
+			keyedChannels = append(keyedChannels, parts[0])
+			keys = append(keys, parts[1])
+		}
+	}
+	if len(keyedChannels) > 0 {
+		joinCommands = append(joinCommands, fmt.Sprintf("JOIN :%s %s", strings.Join(keyedChannels, ","), strings.Join(keys, ",")))
+	}
+	if len(keylessChannels) > 0 {
+		joinCommands = append(joinCommands, fmt.Sprintf("JOIN :%s", strings.Join(keylessChannels, ",")))
+	}
+	return
 }
 
 func publishMessages(em *irc.EventManager, _ *irc.Connection, m *irc.Message) {
